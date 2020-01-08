74c841b7dcbe63a8b552e38e52678d41446b82b8
Austin Johnson <technicallyaustin@gmail.com>
2020-01-08 02:20:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,6 @@
 class Shoe
+attr_accessor :brand, :color, :size, :material, :condition, :cobble
 def initialize(brand)
 end
-attr_accessor :brand, :color, :size, :material, :condition, :cobble
-
   
 end
