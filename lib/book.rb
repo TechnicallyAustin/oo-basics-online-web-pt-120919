b4b23a941239f3b5d7ee90cafc7cc03acebaf76b
Austin Johnson <technicallyaustin@gmail.com>
2020-01-08 02:08:17 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,34 @@
+class Book
+  def initialize(title)
+@title = title
+end
+def title=(title)
+  @title = title
+end
+
+def title
+  @title
+end
+
+def author=(name)
+  @author = name
+end
+
+def author
+  @author
+end
+
+def page_count=(pages)
+  @pages = pages 
+end
+
+def page_count
+  @pages
+end
+
+def next_page
+  @pages 
+end
+end
 
 
